410af3a10eca56dc418bf58155df9606c290c787
Senior <32152485+lingbaoemperor@users.noreply.github.com>
2018-01-16 18:04:52 +0800

    Add files via upload

---
+++ I'mstuck.cpp
@@ -0,0 +1,154 @@
+//I'm stuck!!! 2013-12 question 5
+#include <iostream>
+using namespace std;
+
+typedef struct Grid
+{
+    char c;
+    int s;
+}Grid;
+int si,sj,ei,ej;
+void init(Grid**&,int r,int c);
+void scan(Grid**&,Grid**&,int r,int c);
+void opt(Grid**&,int,int);
+void optchar(Grid**&,int,int);
+void DPS(Grid**&,int,int,int,int);
+int Count(Grid**&,Grid**&,int,int);
+int main()
+{
+    int r,c;
+    cin >>r >>c;
+    Grid** p = new Grid*[r];
+    Grid** q = new Grid*[r];
+    for(int i=0;i < r;i++)
+    {
+        p[i] = new Grid[c];
+        q[i] = new Grid[c];
+    }
+
+    init(p,r,c);
+    init(q,r,c);
+    scan(p,q,r,c);
+    DPS(p,si,sj,r,c);
+    if(p[ei][ej].s == 0)
+    {
+        cout <<"I'm stuck!";
+        return 0;
+    }
+    cout <<Count(p,q,r,c);
+    for(int i=0;i < r;i++)
+    {
+        delete [] p[i];
+        delete [] q[i];
+    }
+    delete p;
+    delete q;
+    return 0;
+}
+void init(Grid**& p,int r,int c)
+{
+    for(int i=0;i < r;i++)
+        for(int j=0;j < c;j++)
+            p[i][j].s = 0;
+}
+void scan(Grid**& p,Grid**& q,int r,int c)
+{
+    string s;
+    getline(cin,s);
+    for(int i=0;i < r;i++)
+    {
+
+        getline(cin,s);
+        for(int j=0;j < c;j++)
+        {
+            if(s[j] == 'S')
+            {
+                si = i;
+                sj = j;
+            }
+            if(s[j] == 'T')
+            {
+                ei = i;
+                ej = j;
+            }
+            p[i][j].c = s[j];
+            q[i][j].c = s[j];
+        }
+    }
+}
+void opt(Grid**& p,int r,int c)
+{
+    for(int i=0;i < r;i++)
+    {
+        for(int j=0;j < c;j++)
+            cout <<p[i][j].s <<" ";
+        cout <<endl;
+    }
+}
+void optchar(Grid**& p,int r,int c)
+{
+    for(int i=0;i < r;i++)
+    {
+        for(int j=0;j < c;j++)
+            cout <<p[i][j].c;
+        cout <<endl;
+    }
+}
+void DPS(Grid**& p,int i,int j,int r,int c)
+{
+    if(p[i][j].c == '#' || p[i][j].s == 1) return;
+    p[i][j].s = 1;
+    switch(p[i][j].c)
+    {
+    case '-':
+        {
+            if(j != 0)
+                DPS(p,i,j-1,r,c);
+            if(j != (c-1))
+                DPS(p,i,j+1,r,c);
+            break;
+        }
+    case '|':
+        {
+            if(i != 0)
+                DPS(p,i-1,j,r,c);
+            if(i != (r-1))
+                DPS(p,i+1,j,r,c);
+            break;
+        }
+    case '.':
+        {
+            if(i != (r-1))
+                DPS(p,i+1,j,r,c);
+            break;
+        }
+    default:
+        {
+            if(j != 0)
+                DPS(p,i,j-1,r,c);
+            if(j != (c-1))
+                DPS(p,i,j+1,r,c);
+            if(i != 0)
+                DPS(p,i-1,j,r,c);
+            if(i != (r-1))
+                DPS(p,i+1,j,r,c);
+            break;
+        }
+    }
+}
+int Count(Grid**& p,Grid**& q,int r,int c)
+{
+    int sum = 0;
+    for(int i=0;i < r;i++)
+        for(int j=0;j < c;j++)
+        {
+            init(q,r,c);
+            if(p[i][j].s == 1 && q[i][j].c != 'T' && q[i][j].c != 'S')
+            {
+                DPS(q,i,j,r,c);
+                if(q[ei][ej].s == 0)
+                    sum++;
+            }
+        }
+    return sum;
+}
---
+++ interestingnumber.cpp
@@ -0,0 +1,30 @@
+//有趣的数 2013-12 question 4
+#include <iostream>
+using namespace std;
+#define X 1000000007
+
+int main3_4_1()
+{
+    int n ;
+    cin >>n;
+    long long**s = new long long*[n+1];
+    for(int i=0;i <= n;i++)
+        s[i] = new long long[6];
+
+    for(int i=0;i < 6;i++)
+        s[0][i] = 0;
+    for(int i=1;i <= n;i++)
+    {
+        s[i][0]=1;
+        s[i][1]=(s[i-1][0]+s[i-1][1]*2)%X;
+        s[i][2]=(s[i-1][1]+s[i-1][2]*2)%X;
+        s[i][3]=(s[i-1][1]+s[i-1][3]*2+s[i-1][4])%X;
+        s[i][4]=(s[i-1][0]+s[i-1][4])%X;
+        s[i][5]=(s[i-1][2]+s[i-1][3]+s[i-1][5]*2)%X;
+    }
+    cout <<s[n][5];
+    for(int i=0;i <= n;i++)
+        delete [] s[i];
+    delete s;
+    return 0;
+}
