2bc93a8714647d6f206aaf17d3126cbbb4375fdd
Senior <32152485+lingbaoemperor@users.noreply.github.com>
2018-10-24 15:39:54 +0800

    Add files via upload

---
+++ 201809-1.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//#include<mem.h>
+//using namespace std;
+//#define MAX_N 1002
+//
+//int N;
+//int price1[MAX_N];
+//int price2[MAX_N];
+//
+//int main()
+//{
+//    memset(price1,0,4*MAX_N);
+//    memset(price2,0,4*MAX_N);
+//    cin >>N;
+//    for(int i=1;i <= N;i++)
+//        cin >>price1[i];
+//    for(int i=1;i <= N;i++)
+//    {
+//        if(i == 1 || i == N)
+//            cout <<(price1[i-1]+price1[i]+price1[i+1])/2 <<" ";
+//        else
+//            cout <<(price1[i-1]+price1[i]+price1[i+1])/3 <<" ";
+//    }
+//    return 0;
+//}
